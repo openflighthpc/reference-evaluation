18323e09e89097fa9817739cce61fabd91eed13b
Kruse Ctrl <ikrusexp@gmail.com>
2022-09-30 15:18:24 +0100

    Add NFS setup checks and etc/hosts check

--- test_setup.sh
+++ test_setup.sh
@@ -21,6 +21,8 @@ remove_item () {
   echo ${output[@]}
 }
 
+# function to check for repo
+
 echo "Testing Begins Now:"
 
 echo "Node names required, enter name or leave blank if all have been named."
@@ -32,22 +34,20 @@ headname="chead1"
 
 echo "Enter COMPUTE node name (leave blank if all named)"
 
-read cnodename
+read cnodeName
 
 # get a list of all the compute nodes (cnodes)
 cnodeList=()
-while [[ $cnodename != ""  ]]
+while [[ $cnodeName != ""  ]]
 do
-	if [[ ! " ${cnodeList[*]} " =~ " ${cnodename} " ]]; then # make duplicate inputs impossible
-		cnodeList+=($cnodename)
+	if [[ ! " ${cnodeList[*]} " =~ " ${cnodeName} " ]]; then # make duplicate inputs impossible
+		cnodeList+=($cnodeName)
 	fi
 	echo "Enter COMPUTE node name (leave blank if all named)"
-	read cnodename
+	read cnodeName
 done
 
 # ---------------------- Testing Zone -------------------------
-
-#remove_item "cnode02" ${cnodeList[*]} 1
 
 #--------------------------------------------------------------
 
@@ -66,13 +66,6 @@ echo "-----------" >> $out
 
 # ---------------------- Testing Zone -------------------------
 
-#temp=$(remove_item "cnode02" ${cnodeList[*]})
-#unset cnodeList
-#cnodeList=${temp[*]}
-#unset temp
-
-#echo "cnodeList now:"
-#echo ${cnodeList[*]}
 
 #--------------------------------------------------------------
 
@@ -121,6 +114,17 @@ fi
 
 echo "($headname) SELinux state: $selinuxState" >> $out
 
+
+# Test 3: Check that head node ip address is the same as in /etc/hosts
+
+headIP=$(hostname -I)
+
+if [[ $(awk "/$headname/{ print NR; exit }" /etc/hosts) != $(awk "/$headIP/{ print NR; exit }" /etc/hosts) ]]; then
+  echo "($headname) not set to correct IP in hosts file, see documenatation page \"Node Setup\"."
+  echo "($headname) IP set incorrectly in /etc/hosts file, should be $headIP"
+fi
+
+
 # Page 2
 
 # Test 1: should be able to ssh into other nodes
@@ -141,7 +145,6 @@ for c in ${cnodeList[@]}; do
     cnodeList=${temp[*]}
     unset temp
     # end remove
-
 	fi
 done
 
@@ -155,6 +158,39 @@ for c in ${cnodeList[@]}; do
     fi
   fi
   echo "($c) SELinux state: $selinuxState" >> $out
+done
+
+
+# Test 3: Check that /etc/hosts file is correct on all nodes
+
+allNodes=("$headname")
+for c in ${cnodeList[@]}; do
+  allNodes+=("$c")
+done
+
+ipList=("$(hostname -I)")
+for c in ${cnodeList[@]}; do # get every ip address
+  ipList+=("$(ssh $c "hostname -I")")
+done
+
+for i in ${!allNodes[@]}; do # check headnode hosts file
+  if [[ $(awk "/${allNodes[$i]}/{ print NR; exit }" /etc/hosts) != $(awk "/${ipList[$i]}/{ print NR; exit }" /etc/hosts) ]]; then
+    echo "($headname) In hosts file, IP error for ${allNodes[$i]}, see documenatation page \"Node Setup\"."
+    echo "($headname) In /etc/hosts, ${allNodes[$i]} IP should be ${allNodes[$i]}" >> $out
+  fi
+done
+
+for c in ${cnodeList[@]}; do # now check compute nodes
+  for i in ${!allNodes[@]}; do # check headnode hosts file
+
+    nodeLine=$(ssh $c "awk '/${allNodes[$i]}/{ print NR; exit }' /etc/hosts")
+    ipLine=$(ssh $c "awk '/${ipList[$i]}/{ print NR; exit }' /etc/hosts")
+
+    if [[ $nodeLine != $ipLine ]]; then
+      echo "($c) In hosts file, IP error for ${allNodes[$i]}, see documenatation page \"Node Setup\"."
+      echo "($c) In /etc/hosts, ${allNodes[$i]} IP should be ${ipList[$i]}" >> $out
+    fi
+  done
 done
 
 
@@ -189,6 +225,7 @@ done
 
 # Page 4: Setup NFS Server
 # this is just for the head node
+# should i check to make sure nfs-utils is installed? probably
 
 # Test 1: check for necessary directories
 # /op/{apps,data,service,site} and /export/{apps,data,service,site}
@@ -202,7 +239,7 @@ for p in ${primeDirs[@]}; do
       echo "($headname) Directory \""/$p/$s/"\" does not exist, see documentation page \"Setup NFS Server\""
       echo "($headname) Directory \""/$p/$s/"\" does not exist." >> $out
     elif [[ $p = export ]]; then
-      perm=$(stat -c %a /$p/$c)
+      perm=$(stat -c %a /$p/$s)
       if [[ ! $perm = 775 ]];then # Test 2: Check that /export/ has the permissions 775
         echo "($headname) Directory \""/$p/$s/"\" has incorrect permissions, see documentation page \"Setup NFS Server\""
         echo "($headname) Directory \""/$p/$s/"\" has permissions $perm - should be 775" >> $out
@@ -210,17 +247,42 @@ for p in ${primeDirs[@]}; do
     fi
   done
 done
+unset primeDirs
+unset subDirs
 
 #Test 3: /etc/exports contains the correct information
+# idea: go through every line until all the content has been found?
 #Test 4: /etc/fstab contains the correct information
 
 #Test 5: systemctl status nfs-server.service is active with no errors
 systemctl status nfs-server.service 1>>/dev/null 2>>$out; result=$?
-if [[ result != 0 ]]; then
+if [[ $result != 0 ]]; then
   echo "($headname) nfs-server.service error, see documentation page \"Setup NFS Server\""
   echo "($headname) nfs-server.service error, system status exit code $result" >> $out
 fi
 
 # Test 6: the correct stuff has been exported showmount --exports
+# Test 6: check if $(showmount --exports --no-header) is the same output as $(showmount -e chead1 --no-header)
+
+headExport=$(showmount --exports --no-header)
+
+for c in ${cnodeList[@]};do
+  nodeExport=$(ssh $c "showmount -e $headname --no-header")
+  if [[ $headExport != $nodeExport ]]; then
+    echo "($headname/$c) Mismatched mount points, see documentation pages: \"Setup NFS Server\" and \"Setup NFS Clients\""
+    echo "($headname/$c) Mismatched mount points: " >> $out
+    echo "$headname export:" >> $out
+    echo "$headExport" >> $out
+    echo "$c export:" >> $out
+    echo "$nodeExport" >> $out
+  fi
+done
+
+# Test 7: check that all compute nodes are mounted
+
+mounted=$(showmount --no-header)
+
+echo $mounted  
+
 
 
