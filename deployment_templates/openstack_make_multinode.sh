d990c75a46fc8ab15fd111268690ea9396ec1027
Kruse Ctrl <ikrusexp@gmail.com>
2022-11-24 15:56:12 +0000

    Add deploy templates and script for openstack

---
+++ deployment_templates/openstack_make_multinode.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "what to name stack?"
+read STACKNAME
+
+echo "key used to access cluster?"
+read KEYFILE
+
+echo "Create standalone cluster"
+openstack stack create --template standalone-template.yaml --parameter "key_name=keytest1" --parameter "flavor=m1.small" --parameter "image=Flight Solo 2022.4" "$STACKNAME"
+
+completed=false
+timeout=120
+while [[ $completed != true ]]; do # just a little loop to not wait an excessive amount of time
+  stack_status=$(openstack stack show "$STACKNAME" -f shell | grep "stack_status")
+  stack_status=${stack_status#*\"} #removes stuff upto // from begining
+  stack_status=${stack_status%\"*} #removes stuff from / all the way to end
+  stack_status=${stack_status%\"*} 
+  stack_status=${stack_status%\"*}
+
+  echo $stack_status
+  if [[ "$stack_status" = 'CREATE_COMPLETE' ]];then
+    completed=true
+  elif [[ $timeout -le 0 ]];then
+    echo "stack creation timed out"
+    exit 1 
+  else
+    let "timeout=timeout-1"
+    sleep 1
+  fi
+done
+
+
+
+pubIP=$(openstack stack output show "$STACKNAME" standalone_public_ip -f shell | grep "output_value")
+pubIP=${pubIP#*\"} #removes stuff upto // from begining
+pubIP=${pubIP%\"*} #removes stuff from / all the way to end
+echo $pubIP
+
+
+privIP=$(openstack stack output show "$STACKNAME" standalone_ip -f shell | grep "output_value")
+privIP=${privIP#*\"} #removes stuff upto // from begining
+privIP=${privIP%\"*} #removes stuff from / all the way to end
+echo $privIP
+
+# now get value of 
+# have to wait for login node to come online
+until ssh -i "$KEYFILE" -o 'StrictHostKeyChecking=no' "flight@$pubIP" 'exit'; do
+  echo "failed?"
+  sleep 5
+done
+
+echo "succeeded?"
+
+contents=$(ssh -i "$KEYFILE" -o 'StrictHostKeyChecking=no' "flight@$pubIP" "sudo /bin/bash -l -c 'echo -n'; sudo cat /root/.ssh/id_alcescluster.pub")
+
+
+echo $contents
+
+openstack stack create --template passwordless-nodes-template.yaml --parameter "key_name=keytest1" --parameter "flavor=m1.small" --parameter "image=Flight Solo 2022.4" --parameter "login_node_ip=$privIP" --parameter "login_node_key=$contents" "compute$STACKNAME"
