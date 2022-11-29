d89a25e83a46501bac0f405a36a77d493700e000
Kruse Ctrl <ikrusexp@gmail.com>
2022-11-29 11:13:01 +0000

    Fix create stack commands

--- deployment_templates/aws_make_multinode.sh
+++ deployment_templates/aws_make_multinode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "what to name stack?"
 read STACKNAME
 
@@ -8,14 +9,16 @@ read KEYFILE
 
 echo "Create standalone cluster"
 #openstack stack create --template standalone-template.yaml --parameter "key_name=keytest1" --parameter "flavor=m1.small" --parameter "image=Flight Solo 2022.4" "$STACKNAME"
-aws cloudformation create-stack --template-body "$(cat aws_standalone.yaml)" --stack-name $STACKNAME --parameters "KeyPair=ivan-keypair,InstanceAmi=ami-04b88ad7b25bc74ef,InstanceSize=t3.small,SecurityGroup=sg-099219b43ee588b21,InstanceSubnet=subnet-55d8582f"
+
+aws cloudformation create-stack --template-body "$(cat aws_standalone.yaml)" --stack-name $STACKNAME --parameters "ParameterKey=KeyPair,ParameterValue=ivan-keypair,UsePreviousValue=false" "ParameterKey=InstanceAmi,ParameterValue=ami-04b88ad7b25bc74ef,UsePreviousValue=false" "ParameterKey=InstanceSize,ParameterValue=t3.small,UsePreviousValue=false" "ParameterKey=SecurityGroup,ParameterValue=sg-099219b43ee588b21,UsePreviousValue=false" "ParameterKey=InstanceSubnet,ParameterValue=subnet-55d8582f,UsePreviousValue=false"
 echo "WORKED?"
 
 aws cloudformation wait stack-create-complete --stack-name $STACKNAME
 
 
-pubIP=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep "PrivateIp" | grep -Pom 1 '[0-9.]{7,15}')
-privIP=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep "PublicIp" | grep -Pom 1 '[0-9.]{7,15}')
+pubIP=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep "PublicIp" | grep -Pom 1 '[0-9.]{7,15}')
+echo "IP of login node: $pubIP"
+privIP=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep "PrivateIp" | grep -Pom 1 '[0-9.]{7,15}')
 
 
 # now get value of 
@@ -32,4 +35,4 @@ contents=$(ssh -i "$KEYFILE" -o 'StrictHostKeyChecking=no' "flight@$pubIP" "sudo
 
 echo $contents
 
-aws cloudformation create-stack --template-body "$(cat aws_multinode.yaml)" --stack-name "compute$STACKNAME" --parameters "KeyPair=ivan-keypair,InstanceAmi=ami-04b88ad7b25bc74ef,InstanceSize=t3.small,SecurityGroup=sg-099219b43ee588b21,InstanceSubnet=subnet-55d8582f,IpData=$privIP,Keydata=$contents"
+aws cloudformation create-stack --template-body "$(cat aws_multinode.yaml)" --stack-name "compute$STACKNAME" --parameters "ParameterKey=KeyPair,ParameterValue=ivan-keypair,UsePreviousValue=false" "ParameterKey=InstanceAmi,ParameterValue=ami-04b88ad7b25bc74ef,UsePreviousValue=false" "ParameterKey=InstanceSize,ParameterValue=t3.small,UsePreviousValue=false" "ParameterKey=SecurityGroup,ParameterValue=sg-099219b43ee588b21,UsePreviousValue=false" "ParameterKey=InstanceSubnet,ParameterValue=subnet-55d8582f,UsePreviousValue=false" "ParameterKey=IpData,ParameterValue=$privIP,UsePreviousValue=false" "ParameterKey=KeyData,ParameterValue=$contents,UsePreviousValue=false"
