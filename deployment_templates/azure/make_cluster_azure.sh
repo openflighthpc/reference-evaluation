63ddd7ed2c716f511fe16ee3054249cd183b1794
Kruse Ctrl <ikrusexp@gmail.com>
2023-01-25 16:18:48 +0000

    add openflight key to all nodes made

--- deployment_templates/azure/make_cluster_azure.sh
+++ deployment_templates/azure/make_cluster_azure.sh
@@ -33,7 +33,11 @@ resourcegroup="resourceful-ivan"
 # Useful info if the setup needs to be slightly changed/ someone else is using these templates
 azurekey="ivan-azure_key"
 
-az deployment group create  --name "$loginname"  --resource-group "$resourcegroup"  --template-file "$logintemplate" --parameters sourceimage=$srcimage clustername=$DEPLOYNAME cheadinstancetype=$logintype ; success=$?
+
+logincloudscript="#cloud-config\nusers:\n  - default\n  - name: flight\n    ssh_authorized_keys:\n    - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWD9MAHnS5o6LrNaCb5gshU4BIpYfqoE2DCW9T2u3v4xOh04JkaMsIzwGc+BNnCh+NlkSE9sPVyPODCVnLnHdyyNfUkLBIUGCM/h9Ox7CTnsbmhnv3tMp4OD2dnGl+wOXWo/0YrWA0cpcl5UchCpZYMGscR4ohg8+/panBJ0//wmQZmCUZkQ20TLumYlL9HdmFl2SO2vraY+nBQCoHtPC80t4BmbPg5atEnQVMngpsRqSykIoUEQKh49t649cF3rBboZT+AmW+O1GWVYu7qlUxqIsdTRJbqbhZ/W2n3rraQh5CR/hOyYikkdn3xqm7Rom5iURvWd6QBh0LhP1UPRIT\n    "
+logindatatrans=$(echo -e "$logincloudscript" | base64 -w0)
+
+az deployment group create  --name "$loginname"  --resource-group "$resourcegroup"  --template-file "$logintemplate" --parameters sourceimage=$srcimage clustername=$DEPLOYNAME cheadinstancetype=$logintype customdatanode="$logindatatrans" ; success=$?
 
 if [[ $STANDALONE != "" ]];then
   echo "standalone node created"
@@ -64,6 +68,7 @@ done
 privIP=$(az vm list -d -o yaml --query "[?name=='$loginname']" | grep "privateIps" | grep -Pom 1 '[0-9.]{7,15}')
 pubIP=$(az vm list -d -o yaml --query "[?name=='$loginname']" | grep "publicIps" | grep -Pom 1 '[0-9.]{7,15}')
 
+
 # now get value of
 # have to wait for login node to come online
 until ssh -i "$keyfile" -o 'StrictHostKeyChecking=no' "flight@$pubIP" 'exit'; do
@@ -75,19 +80,19 @@ echo "succeeded?"
 
 contents=$(ssh -i "$keyfile" -o 'StrictHostKeyChecking=no' "flight@$pubIP" "sudo /bin/bash -l -c 'echo -n'; sudo cat /root/.ssh/id_alcescluster.pub")
 
-echo $contents
+#echo $contents
 
 # so that creates a standalone node, now time to create a multinode thing
 
 ipdata="$privIP"
 keydata="$contents"
-cloudscript="#cloud-config\nwrite_files:\n  - content: |\n      SERVER=$ipdata\n    path: /opt/flight/cloudinit.in\n    permissions: '0644'\n    owner: root:root\nusers:\n  - default\n  - name: root\n    ssh_authorized_keys:\n    - $keydata\n"
-
+cloudscript="#cloud-config\nwrite_files:\n  - content: |\n      SERVER=$ipdata\n    path: /opt/flight/cloudinit.in\n    permissions: '0644'\n    owner: root:root\nusers:\n  - default\n  - name: root\n    ssh_authorized_keys:\n    - $keydata\n  - name: flight\n    ssh_authorized_keys:\n    - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWD9MAHnS5o6LrNaCb5gshU4BIpYfqoE2DCW9T2u3v4xOh04JkaMsIzwGc+BNnCh+NlkSE9sPVyPODCVnLnHdyyNfUkLBIUGCM/h9Ox7CTnsbmhnv3tMp4OD2dnGl+wOXWo/0YrWA0cpcl5UchCpZYMGscR4ohg8+/panBJ0//wmQZmCUZkQ20TLumYlL9HdmFl2SO2vraY+nBQCoHtPC80t4BmbPg5atEnQVMngpsRqSykIoUEQKh49t649cF3rBboZT+AmW+O1GWVYu7qlUxqIsdTRJbqbhZ/W2n3rraQh5CR/hOyYikkdn3xqm7Rom5iURvWd6QBh0LhP1UPRIT\n    "
 cloudtranslat=$(echo -e "$cloudscript" | base64 -w0)
 az deployment group create --name "$DEPLOYNAME" --debug --resource-group "$resourcegroup" --template-file "$computetemplate" --parameters sourceimage="$srcimage" clustername="$DEPLOYNAME" customdatanode="$cloudtranslat" computeNodesCount=2
 #  computeinstancetype="$computetype" adminUsername="$adminname" adminPublicKey="$adminkey"
 
-echo "finished"
-
+echo "Complete."
+echo "Get started by connecting to:"
+echo "$pubIP"
 
  
