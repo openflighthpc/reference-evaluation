d3269a78b7401865b91e6fd1786180a6f80e7038
Cloud User <flight@cram-multi-head-nkbmmnliioog.novalocal>
2023-03-06 18:26:28 +0000

    Add variables

--- regression_tests/environment_variables.sh
+++ regression_tests/environment_variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export allnodescount="1"
-export computenodescount="3"
-export loginip="10.50.0.14"
-export allnodePrivIPs=( "10.50.0.14" )
+export allnodescount="3"
+export computenodescount="2"
+export ip_range="10.50.0.0/16"
+export kube_pod_range="192.168.0.0/16"
+export loginip="10.50.0.32"
+export login_pub_ip="10.151.15.222"
+export allnodePrivIPs=( "10.50.0.32" "10.50.0.48" "10.50.0.21" )
 export varlocation="/home/flight/git/reference-evaluation/regression_tests/environment_variables.sh"
