b3603daef08cbf67f8a346a079388efcd7855cd6
Kruse Ctrl <ikrusexp@gmail.com>
2023-04-05 15:10:47 +0100

    Adjust parent to be more robust

--- deployment_templates/allstack/0_parent.sh
+++ deployment_templates/allstack/0_parent.sh
@@ -6,6 +6,7 @@ source "./1_setup.sh" "$@"; result=$? # run the setup script with all the parame
 case $result in
 
   1)
+    echo "setup failed"
     exit 1
     ;;
   2)
@@ -17,16 +18,18 @@ esac
 
 source "./2_create_standalone.sh"; result=$?
 
-if [[ $result = 1 ]];then
-  exit 1
+if [[ $result != 0 ]];then
+  exit $result
 fi
 
 if [[ $standalone = true ]];then
-  source "./2a_standalone_testing.sh"
+  source "./2a_standalone_testing.sh"; result=$?
   echoplus -v 0 "Login public IP: $login_public_ip"
   echoplus -v 0 "Login private IP: $login_private_ip"
-  exit 0
+  exit $result
 fi
+
+source "./3_create_cnodes.sh"; result=$?
 
 # get exit code and output it to console and log file during creation
 # use success/failure to determin if should auto delete
