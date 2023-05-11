fb848690a728d41ce6ae3ea55f8307476cd30d0f
Cloud User <flight@ip-172-31-28-153.eu-west-2.compute.internal>
2023-05-11 10:11:46 +0000

    Add value to env setup file

--- regression_tests/environment_variables.sh
+++ regression_tests/environment_variables.sh
@@ -2,9 +2,10 @@
 
 export all_nodes_count="3"
 export computenodescount="2"
-export ip_range="10.50.0.0/16"
+export ip_range="172.31.0.0/16"
 export kube_pod_range="192.168.0.0/16"
-export login_priv_ip="10.50.0.32"
-export login_pub_ip="10.151.15.222"
-export all_nodes_priv_ips=( "10.50.0.32" "10.50.0.48" "10.50.0.21" )
+export login_priv_ip="172.31.28.153"
+export login_pub_ip="3.10.141.100"
+export self_pub_ip="3.10.141.100"
+export all_nodes_priv_ips=( "172.31.28.153" "172.31.29.131" "172.31.27.68" )
 export varlocation="/home/flight/git/reference-evaluation/regression_tests/environment_variables.sh"
