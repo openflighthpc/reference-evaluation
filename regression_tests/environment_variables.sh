3fc2a65c29891aa272b53ba628394db5157d3311
Cloud User <flight@kubecram-head-qnak2pby64wn.novalocal>
2023-05-16 14:17:56 +0000

    add dirlocation var to envioronment file

--- regression_tests/environment_variables.sh
+++ regression_tests/environment_variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 export all_nodes_count="3"
 export computenodescount="2"
-export ip_range="172.31.0.0/16"
+export ip_range="10.50.0.0/16"
 export kube_pod_range="192.168.0.0/16"
-export login_priv_ip="172.31.28.153"
-export login_pub_ip="3.10.141.100"
-export self_pub_ip="3.10.141.100"
-export all_nodes_priv_ips=( "172.31.28.153" "172.31.29.131" "172.31.27.68" )
-export varlocation="/home/flight/git/reference-evaluation/regression_tests/environment_variables.sh"
+export login_priv_ip="10.50.0.47"
+export login_pub_ip="10.151.15.229"
+export self_pub_ip="10.151.15.229"
+export all_nodes_priv_ips=( "10.50.0.47" "10.50.0.37" "10.50.0.33" )
+export dirlocation="/home/flight/git/reference-evaluation/regression_tests/"
+export varlocation="${dirlocation}environment_variables.sh"
