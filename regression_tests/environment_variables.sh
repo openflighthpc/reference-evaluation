77084c624fa59cdc69ffbe02037d63384e2e0a4f
Kruse Ctrl <ikrusexp@gmail.com>
2023-05-18 17:16:15 +0100

    Add variables to represent cloudinit options

--- regression_tests/environment_variables.sh
+++ regression_tests/environment_variables.sh
@@ -9,3 +9,6 @@ export self_pub_ip="10.151.15.229"
 export all_nodes_priv_ips=( "10.50.0.47" "10.50.0.37" "10.50.0.33" )
 export dirlocation="/home/flight/git/reference-evaluation/regression_tests/"
 export varlocation="${dirlocation}environment_variables.sh"
+export autoparsematch="false"
+export self_label=""
+export self_prefix=""
